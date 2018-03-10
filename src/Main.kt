631f87f424214911529a169b6a4a805d868ab147
furmansd <furmansd@altarix.ru>
2018-03-10 17:10:11 +0400

    Правки по заданию

--- src/Main.kt
+++ src/Main.kt
@@ -4,18 +4,19 @@ import generator.PseudoRandomGenerator
 fun main(args: Array<String>) {
 
     with(MajorityVoteGenerator(
+            PseudoRandomGenerator(k = 6, p = 2, a = arrayOf(0, 1, 0, 1, 0, 1),
+                    x0 = arrayOf(0, 1, 0, 1, 1, 0)),
             PseudoRandomGenerator(k = 6, p = 2, a = arrayOf(0, 0, 0, 1, 1, 1),
-                    x0 = arrayOf(0, 1, 0, 1, 1, 0))
+                    x0 = arrayOf(1, 0, 0, 1, 1, 0)),
+            PseudoRandomGenerator(k = 6, p = 2, a = arrayOf(0, 0, 0, 0, 1, 1),
+                    x0 = arrayOf(0, 1, 1, 1, 1, 0))
     )
     ) {
         val v = mutableListOf<Long>()
-        for (i in 1..124) {
+        for (i in 1..100000) {
             v.add(next())
-            print("${v.get(i - 1)} ")
-            if (i % 15 == 0) println()
         }
         v.writeSequence()
-        println()
         println("Period = ${v.countPeriod()}")
     }
 
